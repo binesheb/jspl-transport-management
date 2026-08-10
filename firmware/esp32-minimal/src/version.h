87932a4f1860ffd35860a954d063d082b69cddd9
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-08-10 21:37:03 +0530

    feat: add firmware version for OTA

---
+++ firmware/esp32-minimal/src/version.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Firmware version used by the automatic OTA updater.
+// Release builds can override this with -DJSPL_FW_VERSION=\"x.y.z\".
+#ifndef JSPL_FW_VERSION
+#define JSPL_FW_VERSION "1.0.0"
+#endif
